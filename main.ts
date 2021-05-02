d49d954018984928236a1361626c0209e2cbf567
xinapxix <xinapxix@gmail.com>
2021-05-02 18:10:25 -0500

    Terminado el animal favorito

--- main.ts
+++ main.ts
@@ -1,3 +1,50 @@
 basic.forever(function () {
-	
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        . # . # .
+        # # # # #
+        . # . # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # . . .
+        # # . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # . . .
+        # # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(5000)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        . # . # .
+        # # # # #
+        . # . # .
+        `)
+    basic.pause(1000)
 })
